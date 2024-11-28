d8fd01fb1b5ce8628fec9b7183ee4dad114ed303
mostazzita <angelnietomatamoros@gmail.com>
2024-11-28 13:54:35 -0400

    corregir discrepancias en rutas del cliente

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import UserProfile from './pages/usr/UserProfile.jsx';
+import AdminProfile from './pages/adm/AdminProfile.jsx';
+import AdminSettings from './pages/adm/AdminSettings.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/adm/profile" element={<AdminProfile />} >
+        <Route path="/settings" element={<AdminSettings />} />
+      </Route>
+      <Route path="/usr/profile" element={<UserProfile />} />
+
+      <Route></Route>
+
+    </Routes>
   </BrowserRouter>,
 )
